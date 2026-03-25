6d47e7ebc7b3314e2d2b57efe46c3351e502afce
barisbb <86950514+barisbb@users.noreply.github.com>
2026-03-25 09:59:31 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,3 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from pathlib import Path
 from utils.pytorch_models import ResNet50
 from models.poolformer import create_poolformer_s12
 from models.ConvMixer import create_convmixer
@@ -11,25 +8,36 @@ from utils.pytorch_utils import seed_everything
 
 
 def train():
-	seed_everything(42)
-	csv_paths = ["Finland","Ireland","Serbia","Austria", "Belgium", "Lithuania", "Portugal", "Switzerland"] #this means that there are 3 clients that includes the images of a specific country. You can add Austria, Belgium, Lithuania, Portugal, Switzerland
-	epochs = 2
-	communication_rounds = 40
-	channels = 10
-	num_classes = 19
-	#model = create_poolformer_s12(in_chans=channels, num_classes=num_classes)
-	#model = create_mlp_mixer(channels, num_classes)
-	#model = create_convmixer(channels=channels, num_classes=num_classes, pretrained=False)
-    #model = create_poolformer_s12(in_chans=channels, num_classes=num_classes)
-	model = ResNet50("ResNet50", channels=channels, num_cls=num_classes, pretrained=False)
-	global_client = GlobalClient(
-		model=model,
-		lmdb_path="",
-		val_path="",
-		csv_paths=csv_paths,
-	)
-	global_model, global_results = global_client.train(communication_rounds=communication_rounds, epochs=epochs)
-	print(global_results)
+    seed_everything(42)
+
+    # Office-Caltech10 domains = clients
+    csv_paths = ["amazon", "caltech", "dslr", "webcam"]
+
+    epochs = 2
+    communication_rounds = 40
+    channels = 3
+    num_classes = 10
+
+    office_caltech_root = "/home/baris/fedavg/office_caltech_10"   # change this
+
+    # model = create_poolformer_s12(in_chans=channels, num_classes=num_classes)
+    # model = create_mlp_mixer(channels, num_classes)
+    # model = create_convmixer(channels=channels, num_classes=num_classes, pretrained=False)
+    model = ResNet50("ResNet50", channels=channels, num_cls=num_classes, pretrained=False)
+
+    global_client = GlobalClient(
+        model=model,
+        lmdb_path=office_caltech_root,
+        val_path="",
+        csv_paths=csv_paths,
+        num_classes=num_classes,
+    )
+
+    global_model, global_results = global_client.train(
+        communication_rounds=communication_rounds,
+        epochs=epochs
+    )
+    print(global_results)
 
 
 if __name__ == '__main__':
